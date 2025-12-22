dceeac02c33e4631a260edf93b0434d1576e2f3d
Paul Bennett <p@mrpbennett.dev>
2025-12-22 14:56:29 +0000

    adding gh cli

---
+++ install/terminal/app-ghcli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo dnf install -y dnf5-plugins
+sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install -y gh --repo gh-cli
